8a091a13c61fd2d08346b09dd86fa9803fc72313
bob <oleg_bulatov@mail.ru>
2022-05-13 14:06:42 +0400

    добавлен eclient

---
+++ eureka-client/src/main/java/ru/bob/eurekaclient/controler/Controller.java
@@ -0,0 +1,12 @@
+package ru.bob.eurekaclient.controler;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/test")
+    public String test(){
+        return "test";
+    }
+}
